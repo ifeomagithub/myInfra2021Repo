1ddd72ed82b17103877542a86d77f3a76141750f
ifeomagithub <118639230+ifeomagithub@users.noreply.github.com>
2023-07-29 22:53:23 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform_80"
+    bucket = "july-dev-tf-state-bucket"
     key = "main"
     region = "us-east-1"
     dynamodb_table = "my-dynamo-db-table"
